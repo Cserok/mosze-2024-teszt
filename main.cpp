a229517342ca74a26ac3280c36d08914fce946ca
Cserok <gyorimate1297@gmail.com>
2024-09-15 21:10:23 +0200

    Javított

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];                      // NELEMENTS helyett N_ELEMENTS
-    std::cout << '1-100 ertekek duplazasa'            // ' ' helyett " " és a sor végén ;
-    for (int i = 0;)                                  // for ciklusból hiányzik i < N_ELEMENTS; i++
+    int *b = new int[N_ELEMENTS];                      // NELEMENTS helyett N_ELEMENTS
+    std::cout << "1-100 ertekek duplazasa";            // ' ' helyett " " és a sor végén ;
+    for (int i = 0; i < N_ELEMENTS; i++)               // for ciklusból hiányzik i < N_ELEMENTS; i++
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)                           // for ciklusban i helyett i < N_ELEMENTS;
+    for (int i = 0; i < N_ELEMENTS; i++)                // for ciklusban i helyett i < N_ELEMENTS;
     {
-        std::cout << "Ertek:"                         //sor végén ; hiányzik
+        std::cout << "Ertek:" ;                         //sor végén ; hiányzik
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)              // N_ELEMENTS végén , helyett ;
+    for (int i = 0; i < N_ELEMENTS; i++)                // N_ELEMENTS végén , helyett ;
     {
-        atlag += b[i]                                 //sor végén ; hiányzik     
+        atlag += b[i];                                  //sor végén ; hiányzik     
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
